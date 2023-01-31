37e14a740d44eed7bbf9d82876f688847ef217ba
Ethan Jacobs <jacobethan1992@gmail.com>
2023-01-31 16:08:32 -0500

    exercise

---
+++ Methods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Methods
+    {
+        public static int Add(int num1, int num2)
+        {
+
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
+     
+    
+    
+     
+   
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("What is your name");
+var userName=Console.ReadLine();
+Console.WriteLine("What is your favorite band");
+var bandName = Console.ReadLine();
+Console.WriteLine("WHat is your favorite color");
+var userColor = Console.ReadLine();
+Console.WriteLine($"your name is {userName} your favorite band is {bandName} your favorite color is {userColor}");
+
+
